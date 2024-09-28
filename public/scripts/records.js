674510f3aaca82cbd050e02b33ca2d91df803d69
EstradaJesus19 <168051444+EstradaJesus19@users.noreply.github.com>
2024-09-28 14:59:27 -0500

    Added frontend for position filtering

--- public/scripts/records.js
+++ public/scripts/records.js
@@ -265,11 +265,6 @@ const icon = {
 };
 
 function setPositionMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
-    });
-
     selectLocationButton.addEventListener('click', function() {
         isSelectingLocation = !isSelectingLocation;
 
