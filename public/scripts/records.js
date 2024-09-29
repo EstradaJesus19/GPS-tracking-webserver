63ef13bf4864ee42087cab7f52f8340f0497bf79
EstradaJesus19 <168051444+EstradaJesus19@users.noreply.github.com>
2024-09-28 21:00:21 -0500

    Added frontend for position filtering

--- public/scripts/records.js
+++ public/scripts/records.js
@@ -1,7 +1,7 @@
 let map; 
+let polyline;
+let path = [];
 let markers = [];
-let polylines = [];
-let paths = [];
 let circle = null;
 let isSelectingLocation = false;
 let selectedPosition = null;
@@ -34,13 +34,91 @@ function initMap() {
         center: { lat: 10.98, lng: -74.81 },
         zoom: 13,
     });
+
+    polyline = new google.maps.Polyline({
+        strokeColor: '#6309CE',
+        strokeOpacity: 1.0,
+        strokeWeight: 5,
+        icons: [{
+            icon: {
+                path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+                scale: 3,
+                strokeColor: '#6309CE',
+                strokeWeight: 2,
+                fillColor: '#6309CE',
+                fillOpacity: 1.0,
+            },
+            offset: '100%',
+            repeat: '100px'
+        }]
+    });
+
+    polyline.setMap(map);
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const startInput = document.getElementById('startDateTime');
+    const endInput = document.getElementById('endDateTime');
+
+    function getMaxDate() {
+        return new Date();
+    }
+
+    const startFlatpickr = flatpickr(startInput, {
+        enableTime: true,
+        dateFormat: "d-m-Y H:i",
+        time_24hr: true,
+        maxDate: getMaxDate(),
+        onOpen: function() {
+            this.set('maxDate', getMaxDate());
+        },
+        onChange: function (selectedDates) {
+            if (selectedDates.length > 0) {
+                const selectedDate = selectedDates[0];
+                endFlatpickr.set('minDate', selectedDate);
+                endFlatpickr.set('maxDate', getMaxDate());
+                validateTime(startFlatpickr, endFlatpickr); 
+            }
+        }
+    });
+
+    const endFlatpickr = flatpickr(endInput, {
+        enableTime: true,
+        dateFormat: "d-m-Y H:i",
+        time_24hr: true,
+        maxDate: getMaxDate(),
+        onOpen: function() {
+            this.set('maxDate', getMaxDate());
+        },
+        onChange: function (selectedDates) {
+            if (selectedDates.length > 0) {
+                const selectedDate = selectedDates[0];
+                startFlatpickr.set('maxDate', selectedDate);
+                validateTime(startFlatpickr, endFlatpickr); 
+            }
+        }
+    });
+  
+    function validateTime(startPicker, endPicker) {
+        const startDate = startPicker.selectedDates[0];
+        const endDate = endPicker.selectedDates[0];
+
+        if (startDate && endDate && startDate.toDateString() === endDate.toDateString()) {
+            const startTime = startDate.getTime();
+            const endTime = endDate.getTime();
+
+            if (startTime >= endTime) {
+                endPicker.setDate(startDate);
+            }
+        }
+    }
+});
 
 document.getElementById('timeFilterBtn').addEventListener('click', function (e) {
     const startInput = document.getElementById('startDateTime');
     const endInput = document.getElementById('endDateTime');
 
-    e.preventDefault();
+    e.preventDefault(); 
 
     const startTime = convertToDatabaseFormat(startInput.value);
     const endTime = convertToDatabaseFormat(endInput.value);
@@ -48,43 +126,67 @@ document.getElementById('timeFilterBtn').addEventListener('click', function (e) 
     fetch(`/api/filterData?startTime=${encodeURIComponent(startTime)}&endTime=${encodeURIComponent(endTime)}`)
         .then(response => response.json())
         .then(data => {
-            clearMap();
+            clearMap(); 
 
             if (data.length > 0) {
                 const bounds = new google.maps.LatLngBounds();
-                paths = separatePathsByTime(data);  // Separar por intervalos de tiempo
 
-                paths.forEach(path => {
-                    const polyline = new google.maps.Polyline({
-                        path: path,
-                        strokeColor: '#6309CE',
-                        strokeOpacity: 1.0,
-                        strokeWeight: 5,
-                        icons: [{
-                            icon: {
-                                path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-                                scale: 3,
-                                strokeColor: '#6309CE',
-                                strokeWeight: 2,
-                                fillColor: '#6309CE',
-                                fillOpacity: 1.0,
-                            },
-                            offset: '100%',
-                            repeat: '100px'
-                        }]
-                    });
-                    polyline.setMap(map);
-                    polylines.push(polyline);
-
-                    path.forEach(point => {
-                        bounds.extend(point);
-                    });
-
-                    // Añadir marcadores en el inicio y final de cada path
-                    addStartEndMarkers(path[0], path[path.length - 1]);
+                data.forEach(point => {
+                    const latLng = { lat: parseFloat(point.latitude), lng: parseFloat(point.longitude) };
+                    path.push(latLng);
+                    bounds.extend(latLng);
                 });
 
+                polyline = new google.maps.Polyline({
+                    path: path,
+                    strokeColor: '#6309CE',
+                    strokeOpacity: 1.0,
+                    strokeWeight: 5,
+                    icons: [{
+                        icon: {
+                            path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+                            scale: 3,
+                            strokeColor: '#6309CE',
+                            strokeWeight: 2,
+                            fillColor: '#6309CE',
+                            fillOpacity: 1.0,
+                        },
+                        offset: '100%',
+                        repeat: '100px'
+                    }]
+                });
+
+                polyline.setMap(map);
                 map.fitBounds(bounds);
+
+                markers.push(new google.maps.Marker({
+                    position: path[0],
+                    map: map,
+                    icon: {
+                        path: google.maps.SymbolPath.CIRCLE,
+                        scale: 5,
+                        fillColor: "#C3AAff",
+                        fillOpacity: 1,
+                        strokeWeight: 2,
+                        strokeColor: "#6309CE"
+                    },
+                    title: "Start"
+                }));
+
+                markers.push(new google.maps.Marker({
+                    position: path[path.length - 1],
+                    map: map,
+                    icon: {
+                        path: google.maps.SymbolPath.CIRCLE,
+                        scale: 5,
+                        fillColor: "#C3AAff",
+                        fillOpacity: 1,
+                        strokeWeight: 2,
+                        strokeColor: "#6309CE"
+                    },
+                    title: "End"
+                }));
+
             } else {
                 Swal.fire({
                     text: 'No data found in the specified time frame.',
@@ -101,6 +203,7 @@ document.getElementById('timeFilterBtn').addEventListener('click', function (e) 
         })
         .catch(error => {
             clearMap();
+
             Swal.fire({
                 text: 'Error getting filtered data: ' + error,
                 icon: 'error',
@@ -121,6 +224,259 @@ function convertToDatabaseFormat(dateTimeStr) {
     const [year, time] = yearTime.split(' ');
     return `${year}-${month}-${day} ${time}`;
 }
+
+document.getElementById('filterType').addEventListener('change', function (e) {
+    const selectedFilter = e.target.value;
+    document.getElementById('timeFilterForm').style.display = selectedFilter === 'time' ? 'block' : 'none';
+    document.getElementById('positionFilterForm').style.display = selectedFilter === 'position' ? 'block' : 'none';
+    clearMap();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const selectLocationBtn = document.getElementById('selectLocationBtn');
+    const radiusInput = document.getElementById('radiusInput');
+    
+    selectLocationBtn.addEventListener('click', function () {
+        if (!isSelectingLocation) {
+            clearMap()
+            isSelectingLocation = true;
+            selectLocationBtn.textContent = 'Set location';
+            enableMapClick();
+            map.setOptions({ draggableCursor: 'crosshair' }); 
+        } else {
+            if (selectedPosition) {
+                circle.setEditable(false);
+                circle.setDraggable(false);
+                isSelectingLocation = false;
+                selectLocationBtn.textContent = 'Select on map';
+                map.setOptions({ draggableCursor: null }); 
+                disableMapClick(); 
+
+                markers.push(new google.maps.Marker({
+                    position: selectedPosition,
+                    map: map,
+                    icon: {
+                        path: google.maps.SymbolPath.CIRCLE,
+                        scale: 5,
+                        fillColor: "#C3AAff",
+                        fillOpacity: 1,
+                        strokeWeight: 2,
+                        strokeColor: "#6309CE"
+                    },
+                    title: "Center"
+                }));
+
+            } else {
+                Swal.fire({
+                    text: 'Select a location on the map',
+                    icon: 'error',
+                    iconColor: '#6309CE',
+                    confirmButtonText: 'Accept',
+                    confirmButtonColor: '#6309CE',
+                    customClass: {
+                        popup: 'swal2-custom-font',
+                        icon: 'swal2-icon-info-custom'
+                    }
+                });
+            }
+        }
+    });
+
+    radiusInput.addEventListener('input', function () {
+        if (circle) {
+            const newRadius = parseFloat(radiusInput.value);
+            circle.setRadius(newRadius);
+        }
+    });
+});
+
+function enableMapClick() {
+   map.addListener('click', handleMapClick);
+}
+
+function disableMapClick() {
+    google.maps.event.clearListeners(map, 'click');
+}
+
+function handleMapClick(event) {
+    selectedPosition = event.latLng;
+
+    clearMap();
+
+    drawCircle(selectedPosition, parseFloat(radiusInput.value), true); 
+
+    document.getElementById('selectLocationBtn').textContent = 'Set location';
+}
+
+function drawCircle(position, radius, isEditable) {
+    clearMap();
+
+    circle = new google.maps.Circle({
+        center: position,
+        radius: radius,
+        strokeColor: '#6309CE',
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+        fillColor: '#C3AAff',
+        fillOpacity: 0.5,
+        map: map,
+        editable: isEditable,  
+        draggable: isEditable  
+    });
+
+    if (isEditable) {
+        circle.addListener('radius_changed', function () {
+            const updatedRadius = Math.round(circle.getRadius());
+            document.getElementById('radiusInput').value = updatedRadius; 
+        });
+
+        circle.addListener('center_changed', function () {
+            selectedPosition = circle.getCenter(); 
+        });
+    }
+}
+
+function clearMap() {
+    if (polyline) {
+        polyline.setMap(null);
+        polyline = null; 
+    }
+    clearMarkers(); 
+    if (circle) {
+        circle.setMap(null); 
+        circle = null; 
+    }
+    path = []; 
+}
+
+function clearMarkers() {
+    markers.forEach(marker => {
+        marker.setMap(null);
+    });
+    markers = []; 
+}
+
+document.getElementById('positionFilterBtn').addEventListener('click', function (e) {
+    e.preventDefault(); 
+
+    // Verifica que se haya seleccionado una posición y radio
+    if (!selectedPosition || !radiusInput.value) {
+        Swal.fire({
+            text: 'Please select a location and define a radius',
+            icon: 'error',
+            iconColor: '#6309CE',
+            confirmButtonText: 'Accept',
+            confirmButtonColor: '#6309CE',
+            customClass: {
+                popup: 'swal2-custom-font',
+                icon: 'swal2-icon-info-custom'
+            }
+        });
+        return;
+    }
+
+    const position = {
+        latitude: selectedPosition.lat(),
+        longitude: selectedPosition.lng(),
+        radius: parseFloat(radiusInput.value)
+    };
+
+    // Solicita los datos filtrados por posición y radio
+    fetch(`/api/filterDataByPosition?latitude=${position.latitude}&longitude=${position.longitude}&radius=${position.radius}`)
+        .then(response => response.json())
+        .then(data => {
+            clearMap(); 
+
+            if (data.length > 0) {
+                const bounds = new google.maps.LatLngBounds();
+
+                data.forEach(point => {
+                    const latLng = { lat: parseFloat(point.latitude), lng: parseFloat(point.longitude) };
+                    path.push(latLng);
+                    bounds.extend(latLng);
+                });
+
+                polyline = new google.maps.Polyline({
+                    path: path,
+                    strokeColor: '#6309CE',
+                    strokeOpacity: 1.0,
+                    strokeWeight: 5,
+                    icons: [{
+                        icon: {
+                            path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+                            scale: 3,
+                            strokeColor: '#6309CE',
+                            strokeWeight: 2,
+                            fillColor: '#6309CE',
+                            fillOpacity: 1.0,
+                        },
+                        offset: '100%',
+                        repeat: '100px'
+                    }]
+                });
+
+                polyline.setMap(map);
+                map.fitBounds(bounds);
+
+                markers.push(new google.maps.Marker({
+                    position: path[0],
+                    map: map,
+                    icon: {
+                        path: google.maps.SymbolPath.CIRCLE,
+                        scale: 5,
+                        fillColor: "#C3AAff",
+                        fillOpacity: 1,
+                        strokeWeight: 2,
+                        strokeColor: "#6309CE"
+                    },
+                    title: "Start"
+                }));
+
+                markers.push(new google.maps.Marker({
+                    position: path[path.length - 1],
+                    map: map,
+                    icon: {
+                        path: google.maps.SymbolPath.CIRCLE,
+                        scale: 5,
+                        fillColor: "#C3AAff",
+                        fillOpacity: 1,
+                        strokeWeight: 2,
+                        strokeColor: "#6309CE"
+                    },
+                    title: "End"
+                }));
+
+            } else {
+                Swal.fire({
+                    text: 'No data found in the specified area.',
+                    icon: 'info',
+                    iconColor: '#6309CE',
+                    confirmButtonText: 'Accept',
+                    confirmButtonColor: '#6309CE',
+                    customClass: {
+                        popup: 'swal2-custom-font',
+                        icon: 'swal2-icon-info-custom'
+                    }
+                });
+            }
+        })
+        .catch(error => {
+            clearMap();
+
+            Swal.fire({
+                text: 'Error fetching filtered data: ' + error,
+                icon: 'error',
+                iconColor: '#6309CE',
+                confirmButtonText: 'Accept',
+                confirmButtonColor: '#6309CE',
+                customClass: {
+                    popup: 'swal2-custom-font',
+                    icon: 'swal2-icon-info-custom'
+                }
+            });
+            console.error('Error fetching filtered data: ', error);
+        });
+});
 
 function separatePathsByTime(data) {
     const timeThreshold = 60000; // 1 minuto en milisegundos
@@ -181,25 +537,4 @@ function addStartEndMarkers(start, end) {
         },
         title: "End"
     }));
-}
-
-function clearMap() {
-    polylines.forEach(polyline => {
-        polyline.setMap(null);
-    });
-    polylines = [];
-
-    clearMarkers();
-    if (circle) {
-        circle.setMap(null); 
-        circle = null; 
-    }
-    paths = []; 
-}
-
-function clearMarkers() {
-    markers.forEach(marker => {
-        marker.setMap(null);
-    });
-    markers = [];
 }
