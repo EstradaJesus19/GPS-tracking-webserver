7290d023658ac7e20e5034555a8432b658315e7e
OrlaGamer09 <orlandoarroyoo02@gmail.com>
2024-10-05 22:28:04 -0500

    chage dateformat in position window v2

--- public/scripts/records.js
+++ public/scripts/records.js
@@ -671,27 +671,33 @@ function createPathSelector(paths) {
         button.onclick = () => selectPath(index, paths);
         
 
-
+        
         // Format date and time for UX
+        // Función para formatear la fecha en inglés
         function formatDateAndTime(date) {
-        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+            const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-        const day = date.getDate();
-        const month = months[date.getMonth()]; // Obtener el nombre corto del mes en inglés
-        const year = date.getFullYear();
-        const hours = date.getHours().toString().padStart(2, '0'); // Asegura que haya dos dígitos en las horas
-        const minutes = date.getMinutes().toString().padStart(2, '0'); // Asegura que haya dos dígitos en los minutos
+            const day = date.getDate();
+            const month = months[date.getMonth()]; 
+            const year = date.getFullYear();
+            const hours = date.getHours().toString().padStart(2, '0'); 
+            const minutes = date.getMinutes().toString().padStart(2, '0'); 
 
-        return `${day} ${month} ${year} at ${hours}:${minutes}`;
-    }
+            return `${day} ${month} ${year} at ${hours}:${minutes}`;
+        }
 
+
+        const startTimeFormatted2 = formatDateAndTime(startDate);
+        const endTimeFormatted2 = formatDateAndTime(endDate);
+
+        // format date and time for query
         const startDate = new Date(pathInfo.startTime);
         const endDate = new Date(pathInfo.endTime);
-        const startTimeFormatted = formatDateAndTime(startDate);
-        const endTimeFormatted = formatDateAndTime(endDate);
-
-        const timeText = `From ${startTimeFormatted} to ${endTimeFormatted}`;
-        timeText.innerText = `: ${startTimeFormatted} to ${endTimeFormatted}`;
+        const startTimeFormatted = `${startDate.getDate()}-${startDate.getMonth() + 1}-${startDate.getFullYear().toString().slice(-2)} ${startDate.getHours()}:${startDate.getMinutes().toString().padStart(2, '0')}`;
+        const endTimeFormatted = `${endDate.getDate()}-${endDate.getMonth() + 1}-${endDate.getFullYear().toString().slice(-2)} ${endDate.getHours()}:${endDate.getMinutes().toString().padStart(2, '0')}`;
+        
+        const timeText = document.createElement('span');
+        timeText.innerText = `: ${startTimeFormatted2} to ${endTimeFormatted2}`;
         timeText.style.marginLeft = '5px'; 
 
         buttonContainer.appendChild(button);
