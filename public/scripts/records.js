ac85f5a6400b4f1e47cfdb7807d31a881079ea5e
EstradaJesus19 <168051444+EstradaJesus19@users.noreply.github.com>
2024-10-08 22:04:15 -0500

    Fixed frontend

--- public/scripts/records.js
+++ public/scripts/records.js
@@ -803,7 +803,7 @@ function updateMarkerPosition(latLng) {
     };
 
     if (markers.length > 2) {
-        markers[length(markers)-1].setPosition(latLng); 
+        markers[markers.length - 1].setPosition(latLng); 
     } else {
         markers.push(new google.maps.Marker({
             position: latLng,
