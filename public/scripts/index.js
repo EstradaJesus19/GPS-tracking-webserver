58c4d614a3104d0cf5ae7ee61ba3858664d58e0e
EstradaJesus19 <168051444+EstradaJesus19@users.noreply.github.com>
2024-10-09 11:50:08 -0500

    Fixed frontend

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,7 +1,7 @@
 // index.js
-import { initMap } from './map.js';
-import { initStreetView } from './streetView.js';
-import { getApiKeyAndLoadMap, loadLastLocation } from './api.js';
+import { initMap } from './index-scripts/map.js';
+import { initStreetView } from './index-scripts/streetView.js';
+import { getApiKeyAndLoadMap, loadLastLocation } from './index-scripts/api.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     initMap();
