5427ba09d1a091f42fd5b496e29faa66fc9836f2
EstradaJesus19 <168051444+EstradaJesus19@users.noreply.github.com>
2024-10-09 17:30:12 -0500

    Fixed frontend

--- public/scripts/records.js
+++ public/scripts/records.js
@@ -3,12 +3,9 @@ import { linkCalendars } from "./records-scripts/calendars.js";
 import { timeFiltering } from "./records-scripts/time-filtering.js";
 import { positionFiltering } from "./records-scripts/position-filtering.js";
 
-// Define variables
-
+// Init page
 getApiOwner();
 getApiKey();
-
-//Filtering by time
 linkCalendars();
 
 // Filter by time
