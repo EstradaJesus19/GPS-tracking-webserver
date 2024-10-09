4b88033034cc8103c59624cb860d4d6b14a0f361
EstradaJesus19 <168051444+EstradaJesus19@users.noreply.github.com>
2024-10-09 18:02:09 -0500

    Fixed frontend

--- public/scripts/records-scripts/position-filtering.js
+++ public/scripts/records-scripts/position-filtering.js
@@ -162,6 +162,13 @@ function drawCircle(position, radius, isEditable) {
             radiusInput.value = radius;
         });
 
+        google.maps.event.addListener(circle, 'mouseup', function () {
+            selectedPosition = circle.getCenter();
+            filterByPosition(radius, selectedPosition, startTime, endTime);
+            latitudeInput.value = selectedPosition.lat().toFixed(4);
+            longitudeInput.value = selectedPosition.lng().toFixed(4);
+        });
+
         google.maps.event.addListener(circle, 'center_changed', function () {
             if  (!isMouseDown){
                 selectedPosition = circle.getCenter();
