e3858dadc8187cafac04de360849c5dfa73c4699
EstradaJesus19 <168051444+EstradaJesus19@users.noreply.github.com>
2024-10-09 16:32:12 -0500

    Fixed frontend

--- public/scripts/records-scripts/position-filtering.js
+++ public/scripts/records-scripts/position-filtering.js
@@ -1,4 +1,4 @@
-import { map, positionFilteringAction, circle } from './init.js';
+import { map, circle } from './init.js';
 import { startTime, endTime } from './time-filtering.js';
 import { selectPath, createPathSelector, clearMap, clearCircles, clearPolylines, clearMarkers } from '../records.js';
 
