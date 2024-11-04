c56bb2eeaf201df4dec5034344e2c8e75ee2af24
EstradaJesus19 <168051444+EstradaJesus19@users.noreply.github.com>
2024-11-04 15:24:31 -0500

    2 vehicles records

--- public/scripts/records.js
+++ public/scripts/records.js
@@ -2,7 +2,7 @@ import { getApiOwner, getApiKey } from "./records-scripts/init.js";
 import { linkCalendars } from "./records-scripts/calendars.js";
 import { timeFiltering } from "./records-scripts/time-filtering.js";
 import { positionFiltering } from "./records-scripts/position-filtering.js";
-import { manageCarDataVisibility } from "./records-scripts/car-variables.js";
+import { manageCarDataVisibility, selectVehicles } from "./records-scripts/car-variables.js";
 
 // Init page
 getApiOwner();
