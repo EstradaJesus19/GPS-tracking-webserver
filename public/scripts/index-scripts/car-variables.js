0e8d824e7795d4b0230b7f2209c4187bdbb70a11
EstradaJesus19 <168051444+EstradaJesus19@users.noreply.github.com>
2024-11-04 09:47:47 -0500

    Added real time vehicle fetching test1

--- public/scripts/index-scripts/car-variables.js
+++ public/scripts/index-scripts/car-variables.js
@@ -1,4 +1,4 @@
-import { vehiclePaths, loadLastLocation } from './fetch-data.js';
+import { vehiclePaths, fetchLatestData } from './fetch-data.js';
 
 let carDataVisible = true;
 export let currentVehicleId = 1;
@@ -36,7 +36,7 @@ export function manageCarDataVisibility() {
     previousVehicleIcon.addEventListener("click", () => {
         if (currentVehicleId > 1) {
             currentVehicleId--;
-            loadLastLocation(currentVehicleId);
+            fetchLatestData(currentVehicleId);
             updateVehicleDisplay();
         }
     });
@@ -44,7 +44,7 @@ export function manageCarDataVisibility() {
     nextVehicleIcon.addEventListener("click", () => {
         if (currentVehicleId < totalVehicles) {
             currentVehicleId++;
-            loadLastLocation(currentVehicleId);
+            fetchLatestData(currentVehicleId);
             updateVehicleDisplay();
         }
     });
