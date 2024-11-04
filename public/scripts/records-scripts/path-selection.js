b9003679aa7e7fbd9aa2ef7921e8992557f37d60
EstradaJesus19 <168051444+EstradaJesus19@users.noreply.github.com>
2024-11-04 18:03:36 -0500

    2 vehicles records

--- public/scripts/records-scripts/path-selection.js
+++ public/scripts/records-scripts/path-selection.js
@@ -309,8 +309,9 @@ document.addEventListener('keydown', (event) => {
 });
 
 function updateMarkerPosition(latLng) {
+    const selectedVehicle = vehiclePathSelector.value;
     const icon = {
-        url: urls[vehicleId],
+        url: urls[selectedVehicle],
         scaledSize: new google.maps.Size(40, 40),
         anchor: new google.maps.Point(20, 35)
     };
