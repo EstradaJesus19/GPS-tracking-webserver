7fb0f47a6ddabb6254c3a60574f6a45c67c4d156
EstradaJesus19 <168051444+EstradaJesus19@users.noreply.github.com>
2024-11-17 07:24:55 -0500

    Fixed center map in real time

--- public/scripts/index-scripts/init.js
+++ public/scripts/index-scripts/init.js
@@ -47,7 +47,7 @@ function initMap() {
     setInterval(() => {
         fetchLatestData(1);
         fetchLatestData(2);
-    }, 5000); 
+    }, 2000); 
 }
 
 function toggleVehicleVisibility(vehicleId, visible) {
